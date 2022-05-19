a1888602afbd2d9e8d716d6e8a10ab56feec775c
Tony Liu <tl0226yn@gmail.com>
2022-05-19 13:20:03 -0700

    initial commit

---
+++ client/src/App.svelte
@@ -0,0 +1,38 @@
+<script>
+	let num = -1;
+	const names = ["Aniruddha Kashyap", "Nolan Breen", "Matthew Johnson", "Cindy Li", "Kat Evers",
+"Alex Hussain", "Edwina Ng", "Albert Cheng", "Jade Henzie", "Smol pp", "nigga"]
+	function getRand() {
+		fetch ("./rand")
+		.then(d => d.text())
+		.then(d => (num = names[d]));
+
+	}
+</script>
+
+<main>
+	<p>Hello {num}!</p>
+	<button on:click={getRand}>Get a random number</button>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import Flask, send_from_directory
+import random
+
+app = Flask(__name__)
+
+@app.route("/")
+def base():
+    return send_from_directory('client/public', 'index.html')
+
+# Path for all the static files (compiled JS/CSS, etc.)
+@app.route("/<path:path>")
+def home(path):
+    return send_from_directory('client/public', path)
+
+@app.route("/rand")
+def hello():
+    return str(random.randint(0, 10))
+
+if __name__ == "__main__":
+    app.run(debug=True)
