27d2b4d79920fbf2076239f6db70cb63390b48f4
Tony Liu <tl0226yn@gmail.com>
2022-05-23 23:28:19 -0700

    finish home page

--- client/src/home.svelte
+++ client/src/home.svelte
@@ -14,6 +14,9 @@
 <div transition:fade class='card'>
     <div class="about-me">
         <h1>Yunxiao Liu</h1>
+        <svg viewbox="0 0 100 1">
+            <line class="lines" x1="0" y1="0" x2="100" y2="0" stroke="teal" stroke-dasharray=100 stroke-width=0.2/>
+        </svg>
         <div class="flex">
             <div class="image">
                 <img src="https://lh3.googleusercontent.com/a-/AOh14Ghp-NOXwc_8kDyCBfR-NGWyKr2Evcrvu2B0DBXJjA=s96-c-rg-br100" alt="pfp" width=100%/>
@@ -35,9 +38,9 @@
     <div class="contact">
         <div class="flex">
             <div class="resume">
-                <h1>My Resume:</h1>
+                <h1>My resume:</h1>
                 <div class="btn-row">
-                    <a class="resbutton" href="https://drive.google.com/file/d/1NVbkS19h9TuKVyNIhkp1MdNBIe0BNJAJ/view?usp=sharing">RESUME</a>
+                    <a href="https://drive.google.com/file/d/1NVbkS19h9TuKVyNIhkp1MdNBIe0BNJAJ/view?usp=sharing"><i class="fa fa-file"></i></a>
                 </div>
             </div>
             <div class="socials">
@@ -47,15 +50,25 @@
                 <a href="https://github.com/TonyLiu0226"><i class="fa fa-github"></i></a>
                 <a href="https://www.instagram.com/tony_26l/?hl=en/"><i class="fa fa-instagram"></i></a>
                 <a href="https://www.youtube.com/channel/UCYLZDU2vS-FeL1E2YaWrG3w"><i class="fa fa-youtube"></i></a>
+                
                 </div>
             </div>
         </div>
     </div>
 </div>
 
-<div class="demo-wrapper">
-    <div class="square">
-        <p>Gay</p>
+<div class="card">
+    <h1>LANGUAGES AND FRAMEWORKS</h1>
+    <div class="languages">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/640px-Python-logo-notext.svg.png" width=33% alt="python">
+        <img src="https://eu-images.contentstack.com/v3/assets/blt66983808af36a8ef/blt7d6dd0195aec2967/614b535dd8068d7efec4da52/Java_logo.png?quality=80&format=jpg&width=640" width=33% alt="java">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/C_Programming_Language.svg/695px-C_Programming_Language.svg.png" width=33% alt="c">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png" width=33% alt="javascript">
+        <img src="https://myiotmart.files.wordpress.com/2016/07/verilog.jpg" width=33% alt="verilog">
+        <img src="https://www.logolynx.com/images/logolynx/00/00429ca224699ddf60ce05b46ef08709.jpeg" width=33% alt="flask">
+        <img src="https://pbs.twimg.com/profile_images/1121395911849062400/7exmJEg4_400x400.png" width=33% alt="svelte">
+        <img src="https://cdn.hashnode.com/res/hashnode/image/upload/v1647490619965/P1dsNgj-f1.png" width=33% alt="react">
+        <img src="https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2,f_auto,g_center,q_auto:good/v1/gcs/platform-data-mongodb/events/mon.png" width=33% alt="mongodb">
     </div>
 </div>
 <style>
@@ -79,6 +92,12 @@
 
 .card:hover {
     transform:translateY(-20px);
+    transform:scale(105%);
+}
+
+.card:hover > .lines {
+    animation: dash 5s linear alternate infinite;
+    color:green;
 }
 
 h1{
@@ -99,8 +118,7 @@ p{
 }
 
 .resume {
-    width: 35%;
-    margin-left: 2.5%;
+    width: 40%;
     margin-right: 2.5%;
 }
 
@@ -112,7 +130,6 @@ p{
 
 .socials {
     width: 55%;
-    margin-left: 2.5%;
     margin-right: 2.5%;
 }
 
@@ -143,6 +160,21 @@ a:hover {
 }
 
 .btn-row {
-    margin-top: 10%;
+    margin-top: 5%;
+    margin-bottom: 5%;
 }
+
+.languages {
+    display:grid;
+    gap: 20px;
+    grid-template-columns: auto auto auto;
+    align-content:center;
+}
+
+img {
+    display:block;
+    margin-left:auto;
+    margin-right:auto;
+}
+
 </style>
