b197a1f0900c88d661c9c13b5cf0bb95a1cee7da
Tony Liu <tl0226yn@gmail.com>
2022-06-16 23:58:23 -0700

    odds and evens

--- client/src/projects.svelte
+++ client/src/projects.svelte
@@ -11,7 +11,7 @@ animation={'fade-in 1.0s cubic-bezier(0.35, 0.5, 0.65, 0.95) both'}>
 <Rickyboi></Rickyboi>
 </Saos>
 <Saos 
-animation={'fade-in 1.0s cubic-bezier(0.35, 0.5, 0.65, 0.95) both'}>
+animation={'fade-in2 1.0s cubic-bezier(0.35, 0.5, 0.65, 0.95) both'}>
 <THC212NT></THC212NT>
 </Saos>
 <Saos 
@@ -19,7 +19,7 @@ animation={'fade-in 1.0s cubic-bezier(0.35, 0.5, 0.65, 0.95) both'}>
 <SRM></SRM>
 </Saos>
 <Saos 
-animation={'fade-in 1.0s cubic-bezier(0.35, 0.5, 0.65, 0.95) both'}>
+animation={'fade-in2 1.0s cubic-bezier(0.35, 0.5, 0.65, 0.95) both'}>
 <UniMeetups></UniMeetups>
 </Saos>
 </div>
@@ -34,6 +34,17 @@ animation={'fade-in 1.0s cubic-bezier(0.35, 0.5, 0.65, 0.95) both'}>
     @keyframes -global-fade-in {
     0% {
       transform: rotateY(-180deg) translateX(-100vw) skewX(0deg);
+      opacity: 0;
+    }
+    100% {
+      transform: rotateY(0deg) translateX(0) skewX(0deg);
+      opacity: 1;
+    }
+  }
+
+  @keyframes -global-fade-in2 {
+    0% {
+      transform: rotateY(180deg) translateX(100vw) skewX(0deg);
       opacity: 0;
     }
     100% {
